bef53ef202bdd6af46378a8c5176e731d907323f
pasmurnoff <alexander.pasmurnov@yandex.ru>
2021-03-19 22:04:46 +0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Подарок №1',
+    price: 799,
+    description: 'В набор входят: кашемировый шарф, авторская ароматизированная свеча, молочный шоколад, подарочная упаковка+оформление'
+  },
+  {
+    name: 'Подарок №2',
+    price: 699,
+    description: 'Плетеная корзина из ассорти бельгийского шоколада, шоколада Ferrero,merci и конфет'
+  },
+  {
+    name: 'Подарок №3',
+    price: 299,
+    description: 'Верх, верхняя деталь гардероба или одежды'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
